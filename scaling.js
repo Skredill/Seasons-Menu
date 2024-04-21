e1dd5891a0a6e9254f64f6b62db7f548f7c5fb34
skredil <dylandifoggio@proton.me>
2024-04-20 20:25:04 -0400

    rewrote header to use flexbox to better account for device scaling

---
+++ scaling.js
@@ -0,0 +1,9 @@
+var Height = $( window ).height();
+var Width = $( window ).width();
+console.log('Width =' , Width , "Height =" , Height);
+
+var navCont = document.getElementById('navCont');
+var dropDown = document.getElementById('dropDown');
+var dropBtn = document.getElementById('dropBtn');
+
+
