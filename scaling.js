fbf335dfab6a8222d18de1169db9464b00351e1b
Skredil <dylandifoggio@proton.me>
2024-05-01 22:33:15 -0400

    desktop and mobile layout almost complete, working on header

--- scaling.js
+++
@@ -1,9 +0,0 @@
-var Height = $( window ).height();
-var Width = $( window ).width();
-console.log('Width =' , Width , "Height =" , Height);
-
-var navCont = document.getElementById('navCont');
-var dropDown = document.getElementById('dropDown');
-var dropBtn = document.getElementById('dropBtn');
-
-
